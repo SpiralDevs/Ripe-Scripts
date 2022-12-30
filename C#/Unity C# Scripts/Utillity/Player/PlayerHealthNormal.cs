9e6d6cf202a6787257d42fa65a322c24c54ecd1d
Spiral <80377533+SpiralGaming@users.noreply.github.com>
2022-12-29 22:40:09 -0500

    Create PlayerHealthNormal.cs

---
+++ C#/Unity C# Scripts/Utillity/Player/PlayerHealthNormal.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+using TMPro;
+
+public class PlayerHealthNormal : MonoBehaviour
+{
+    [Header("Main Health")]
+    public int maxHealth = 10;
+    public int currentHealth;
+    public GameObject HealthSlider;
+    public TMP_Text healthText;
+
+    [Header("Player")]
+    public GameObject playerBody;
+    Vector3 current;
+
+    void Start()
+    {
+        currentHealth = 10;  
+    }
+
+    private void Update() 
+    {
+        if(currentHealth <= 0)
+        {
+            current = playerBody.transform.position;
+            playerBody.GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            current = playerBody.transform.position;
+            playerBody.GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
