946f20f6ee051b744c943ac930ce142c3103ecfa
Spiral <80377533+SpiralGaming@users.noreply.github.com>
2024-07-10 20:16:55 -0700

    Update LoadScenes.cs

--- C#/Unity C# Scripts/Basics/LoadScenes.cs
+++ C#/Unity C# Scripts/Basics/LoadScenes.cs
@@ -5,11 +5,11 @@ public class LoadScenes : MonoBehaviour
 {
     public void SceneString()
     {
-        /*Loading Example 1: String */ SceneManager.LoadScene("SampleScene")//place the name of the scene you want to use in the quotations
+        /*Loading Example 1: String */ SceneManager.LoadScene("SampleScene");//place the name of the scene you want to use in the quotations
     }
     public void SceneInt()
     {
-        /*Loading Example 2: Int/Build Index*/ SceneManager.LoadScene(1)//In the parentheses is the build index of the scene in File > Build Settings
+        /*Loading Example 2: Int/Build Index*/ SceneManager.LoadScene(1);//In the parentheses is the build index of the scene in File > Build Settings
     }
 
     void Start()
